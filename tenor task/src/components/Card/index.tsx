60e97b09935bf64575f0e173be2131d4a15712cd
Hari <hariharang25@gmail.com>
2022-07-10 19:15:58 +0530

    remove: zip files

--- tenor task/src/components/Card/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./index.module.scss";
-
-const Card = (props: any) => {
-    return <div className={`${props?.className} ${styles.card}`}>{props?.children}</div>;
-};
-
-export default Card;
